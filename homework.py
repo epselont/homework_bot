57c40352557c9e969fc2298bfecf9189a2623897
Роман Корабельников <newman13@yandex.ru>
2022-04-25 11:32:40 +0300

    ready to submit for review

--- homework.py
+++ homework.py
@@ -31,10 +31,11 @@ logging.basicConfig(
 
 
 def send_message(bot, message):
-    bot.send_message(
-        TELEGRAM_CHAT_ID,
-        message
-    )
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        logging.info(f'Сообщение: "{message}" удачно отправлено!')
+    except Exception as error:
+        logging.error(f'Ошибка при отправке сообщени: {error}')
 
 
 def get_api_answer(current_timestamp):
@@ -55,7 +56,7 @@ def check_response(response):
     if isinstance(response, dict) and isinstance(response['homeworks'], list):
         return response.get('homeworks')
     else:
-        logging.error('ERROR')
+        logging.error('Отсутствуют ожидаемые ключи в ответе API')
         raise TypeError
 
 
@@ -66,6 +67,7 @@ def parse_status(homework):
         raise KeyError
     homework_status = homework.get('status')
     if homework_status not in HOMEWORK_STATUSES.keys():
+        logging.error(f'Неизвестный статус домашней работы {homework_status}')
         raise AssertionError
     verdict = HOMEWORK_STATUSES[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
@@ -83,30 +85,25 @@ def check_tokens():
 
 def main():
     """Основная логика работы бота."""
-    current_timestamp = int(time.time()) - (ONE_DAY_IN_SEC * DAYS)
-    response = get_api_answer(current_timestamp)
-    ch = check_response(response)
-    ps = parse_status(ch[0])
+    if check_tokens():
+        bot = Bot(token=TELEGRAM_TOKEN)
+        current_timestamp = int(time.time()) - (ONE_DAY_IN_SEC * DAYS)
+        status = ''
+        while True:
+            try:
+                response = get_api_answer(current_timestamp)
+                homework = check_response(response)
+                message = parse_status(homework)
+                if status != message:
+                    send_message(bot, message)
+                    status = message
+            except Exception as error:
+                message = f'Сбой в работе программы: {error}'
+                logging.error(message)
+                send_message(bot, message)
+            finally:
+                time.sleep(RETRY_TIME)
 
 
-#
-#    bot = Bot(token=TELEGRAM_TOKEN)
-#    current_timestamp = int(time.time())
-#
-#    while True:
-#        try:
-#            response = get_api_answer()
-#
-#            # current_timestamp =
-#            time.sleep(RETRY_TIME)
-#
-#        except Exception as error:
-#            message = f'Сбой в работе программы: {error}'
-#
-#            time.sleep(RETRY_TIME)
-#        else:
-#            pass
-#
-#
 if __name__ == '__main__':
     main()
